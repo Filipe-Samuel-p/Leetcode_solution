f4f737048827f468baae53372a7539e313f74911
Filipe Samuel <felipepires.p125@gmail.com>
2024-05-06 00:32:52 -0300

    palindrome better

--- 06 Palindrome_Number.py
+++ 06 Palindrome_Number.py
@@ -13,11 +13,6 @@ Explanation: 121 reads as 121 from left to right and from right to left."""
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         string = str(x)
-        lista = [x for x in string]
-        lista2 = []
-        for i in range(len(lista) - 1, -1, -1):
-            lista2.append(lista[i])
-        palindrome = "".join(lista2)
-        valor = True if palindrome == string else False
+        value = True if string == string[::-1] else False
         
-        return valor
+        return value
